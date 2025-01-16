ae5647f7dfc90b8b4adad9aa8d50435a4951415d
Prananda Yoga <pranandayoga21@gmail.com>
2025-01-16 15:59:01 +0800

    feat: add app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConsumerController } from './consumer/consumer.controller';
+import { ConsumerService } from './consumer/consumer.service';
+import { CustomerController } from './customer/customer.controller';
+import { CustomerService } from './customer/customer.service';
+import { ProviderController } from './provider/provider.controller';
+import { ProviderService } from './provider/provider.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [
+    AppController,
+    ConsumerController,
+    CustomerController,
+    ProviderController,
+  ],
+  providers: [AppService, ConsumerService, CustomerService, ProviderService],
 })
 export class AppModule {}
