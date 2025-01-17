1551a7163fd6cdc8e8190d10ef235f8040e7bec5
Prananda Yoga <pranandayoga21@gmail.com>
2025-01-17 15:31:57 +0800

    docs: add swagger config on main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,20 @@ async function bootstrap() {
     }),
   );
 
+  const options = new DocumentBuilder()
+    .setTitle('Microfinance API')
+    .setDescription(
+      'Documentation of API that interact on Microfinance based on DJoin Case',
+    )
+    .setVersion('1.0.0')
+    .addTag('Consumer')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Application running on: ${await app.getUrl()}`);
 }
 bootstrap();
