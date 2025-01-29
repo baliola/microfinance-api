5f66e61b1a6480250113ee4cc239ad29297279a6
Prananda Yoga <pranandayoga21@gmail.com>
2025-01-29 21:57:47 +0800

    feat<env.type, type>: add type for env and custom type

--- src/utils/type/type.ts
+++ src/utils/type/type.ts
@@ -1,3 +1,5 @@
 export type TransactionType = 'PENDING' | 'APPROVED' | 'REJECTED';
 export type TransactionRequestType = 'PENDING';
 export type TransactionResponseType = 'APPROVED' | 'REJECTED';
+
+export type WalletAddressType = `0x${string}`;
