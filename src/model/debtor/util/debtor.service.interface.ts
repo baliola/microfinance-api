66b9e8d864b1e3af3f2c8087b9c27f1969e1a2a5
Prananda Yoga <pranandayoga21@gmail.com>
2025-01-31 15:37:46 +0800

    feat<debtor.type, debtor.interface>: add type for response type on debtor controller and service, adjust interface on debtor service based on ethers service implementation

--- src/model/debtor/util/debtor.service.interface.ts
+++ src/model/debtor/util/debtor.service.interface.ts
@@ -1,4 +1,7 @@
+import { LogActivityType } from '../../debtor/util/debtor-type.service';
+import { RegistrationServiceType } from './debtor-type.service';
+
 export interface IDebtorService {
-  getLogActivity(nik: string): Promise<void>;
-  registration(nik: string): Promise<void>;
+  getLogActivity(nik: string): Promise<LogActivityType>;
+  registration(nik: string): Promise<RegistrationServiceType>;
 }
