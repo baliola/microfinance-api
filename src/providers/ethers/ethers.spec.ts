840c59af5d1bc95c1697d20e831d49b3b50eb2bc
Prananda Yoga <pranandayoga21@gmail.com>
2025-02-10 17:06:19 +0800

    test<ethers.spec>: adjust test script on ethers provider

--- src/providers/ethers/ethers.spec.ts
+++ src/providers/ethers/ethers.spec.ts
@@ -3,6 +3,16 @@ import { EthersService } from './ethers';
 
 describe('Ethers', () => {
   let provider: EthersService;
+  const mockEtherService = {
+    generateWallet: jest.fn(),
+    getEIP712Domain: jest.fn(),
+    signMetaTransaction: jest.fn(),
+    executeMetaTransaction: jest.fn(),
+    addCreditor: jest.fn(),
+    addCreditorWithEvent: jest.fn(),
+    addDebtor: jest.fn(),
+    removeCreditor: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
