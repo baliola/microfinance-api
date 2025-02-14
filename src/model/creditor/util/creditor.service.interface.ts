fb416ea3591f2a91dbc2ad7a3898676483657e27
Prananda Yoga <pranandayoga21@gmail.com>
2025-02-14 10:29:56 +0800

    fix<creditor.service.interface>: adjust type on PurchasePackageType

--- src/model/creditor/util/creditor.service.interface.ts
+++ src/model/creditor/util/creditor.service.interface.ts
@@ -3,7 +3,7 @@ import {
   AddDebtorToCreditorType,
   CreateDelegationType,
   DelegationApprovalType,
-  PurcahsePackageType,
+  PurchasePackageType,
   RegistrationServiceType,
 } from './creditor-type.service';
 
@@ -59,5 +59,5 @@ export interface ICreditorService {
     start_date: string,
     end_date: string,
     quota: number,
-  ): Promise<PurcahsePackageType>;
+  ): Promise<PurchasePackageType>;
 }
