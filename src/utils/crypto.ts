3743f9bc9815d163174a48375d1efdca728738a6
Prananda Yoga <pranandayoga21@gmail.com>
2025-02-15 16:33:56 +0800

    fix<crypto>: improve mechanism for encrypt and decrypt for simple value passing

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -4,11 +4,9 @@ import * as crypto from 'node:crypto';
 const ALGORITHM = 'aes-256-gcm';
 const KEY_LENGTH = 32;
 const IV_LENGTH = 16;
+const AUTH_TAG_LENGTH = 16;
 
-export function encrypt(
-  text: string,
-  secret: string,
-): { iv: string; encryptedData: string; authTag: string } {
+export function encrypt(text: string, secret: string): string {
   const key = Buffer.from(secret, 'hex');
   if (key.length !== KEY_LENGTH) {
     throw new InternalServerErrorException(
@@ -21,29 +19,33 @@ export function encrypt(
 
   let encrypted = cipher.update(text, 'utf-8', 'hex');
   encrypted += cipher.final('hex');
-  const authTag = cipher.getAuthTag().toString('hex');
+  const authTag = cipher.getAuthTag();
 
-  return {
-    iv: iv.toString('hex'),
-    encryptedData: encrypted,
-    authTag,
-  };
+  // Concatenate IV, authTag, and encrypted data into a single string (hex)
+  return iv.toString('hex') + authTag.toString('hex') + encrypted;
 }
 
-export function decrypt(
-  encrypted: { iv: string; encryptedData: string; authTag: string },
-  secret: string,
-): string {
-  if (secret.length !== KEY_LENGTH) {
-    throw new Error(`Secret key must be ${KEY_LENGTH} bytes long`);
+export function decrypt(encryptedData: string, secret: string): string {
+  if (secret.length !== KEY_LENGTH * 2) {
+    throw new InternalServerErrorException(
+      `Secret key must be ${KEY_LENGTH} bytes long`,
+    );
   }
 
-  const iv = Buffer.from(encrypted.iv, 'hex');
-  const key = Buffer.from(secret, 'utf-8');
-  const decipher = crypto.createDecipheriv(ALGORITHM, key, iv);
-  decipher.setAuthTag(Buffer.from(encrypted.authTag, 'hex'));
+  const key = Buffer.from(secret, 'hex');
+  const iv = Buffer.from(encryptedData.substring(0, IV_LENGTH * 2), 'hex');
+  const authTag = Buffer.from(
+    encryptedData.substring(IV_LENGTH * 2, IV_LENGTH * 2 + AUTH_TAG_LENGTH * 2),
+    'hex',
+  );
+  const encryptedText = encryptedData.substring(
+    IV_LENGTH * 2 + AUTH_TAG_LENGTH * 2,
+  );
 
-  let decrypted = decipher.update(encrypted.encryptedData, 'hex', 'utf-8');
+  const decipher = crypto.createDecipheriv(ALGORITHM, key, iv);
+  decipher.setAuthTag(authTag);
+
+  let decrypted = decipher.update(encryptedText, 'hex', 'utf-8');
   decrypted += decipher.final('utf-8');
 
   return decrypted;
