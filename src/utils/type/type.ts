11ceeca25da50f964fd91a22bcc08e2edc429233
Prananda Yoga <pranandayoga21@gmail.com>
2025-02-17 17:01:11 +0800

    fix<type>: adjust type on TransactionType and add TransactionCommonType type

--- src/utils/type/type.ts
+++ src/utils/type/type.ts
@@ -1,6 +1,7 @@
-export type TransactionType = 'PENDING' | 'APPROVED' | 'REJECTED';
+export type TransactionType = 'NONE' | 'PENDING' | 'APPROVED' | 'REJECTED';
 export type TransactionRequestType = 'PENDING';
 export type TransactionResponseType = 'APPROVED' | 'REJECTED';
+export type TransactionCommonType = 'PENDING' | 'APPROVED' | 'REJECTED';
 
 export type WalletAddressType = `0x${string}`;
 
