cb73c214ad8b13b8bb75447f286179db24fe5854
Prananda Yoga <pranandayoga21@gmail.com>
2025-02-28 09:59:41 +0800

    test<app.e2e-spec>: add integration test for registration debtor

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -15,10 +15,22 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('/api/debtor/registration (POST)', async () => {
+    return await request(app.getHttpServer())
+      .post('/api/debtor/registration')
+      .send({
+        debtor_nik: '5101010',
+      })
+      .expect(201)
+      .expect((res) => {
+        expect(res.body).toEqual(
+          expect.objectContaining({
+            data: expect.objectContaining({
+              wallet_address: expect.stringMatching(/^0x[a-fA-F0-9]{40}$/),
+            }),
+            message: 'Debtor registration success.',
+          }),
+        );
+      });
   });
 });
